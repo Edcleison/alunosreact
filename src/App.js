d1021cf39a5dc3e3bc0752a48dea1a02d898300e
Edcleison <edcleisonbrito@gmail.com>
2023-11-10 23:39:00 -0300

    jwt

--- src/App.js
+++ src/App.js
@@ -10,6 +10,35 @@ function App() {
   const baseUrl = "https://localhost:44396/api/alunos";
 
   const [data, setData] = useState([]);
+  const [modalIncluir, setModalIncluir] = useState(false);
+  const [modalEditar, setModalEditar] = useState(false);
+  const [modalExcluir, setModalExcluir] = useState(false);
+  const [updateData, setUpdateData] = useState(true);
+
+  const [alunoSelecionado, setAlunoSelecionado] = useState({
+    id: '',
+    nome: '',
+    email: '',
+    idade: ''
+  })
+
+  const abrirFecharModalIncluir = () => {
+    setModalIncluir(!modalIncluir);
+  }
+  const abrirFecharModalEditar = () => {
+    setModalEditar(!modalEditar);
+  }
+  const abrirFecharModalExcluir = () => {
+    setModalExcluir(!modalExcluir);
+  }
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setAlunoSelecionado({
+      ...alunoSelecionado, [name]: value
+    });
+    console.log(alunoSelecionado);
+  }
 
   const pedidoGet = async () => {
     await axios.get(baseUrl)
@@ -19,19 +48,67 @@ function App() {
         console.log(error);
       })
   }
+  const pedidoPost = async () => {
+    delete alunoSelecionado.id;
+    alunoSelecionado.idade = parseInt(alunoSelecionado.idade);
+    await axios.post(baseUrl, alunoSelecionado)
+      .then(response => {
+        setData(data.concat(response.data));
+        setUpdateData(true);
+        abrirFecharModalIncluir();
+      }).catch(error => {
+        console.log(error);
+      })
+  }
+  const pedidoPut = async () => {
+    alunoSelecionado.idade = parseInt(alunoSelecionado.idade);
+    await axios.put(baseUrl + "/" + alunoSelecionado.id, alunoSelecionado)
+      .then(response => {
+        var resposta = response.data;
+        var dadosAuxiliar = data;
+        dadosAuxiliar.map(aluno => {
+          if (aluno.id === alunoSelecionado.id) {
+            aluno.nome = resposta.nome;
+            aluno.email = resposta.email;
+            aluno.idade = resposta.idade;
+          }
+        });
+        setUpdateData(true);
+        abrirFecharModalEditar();
+      }).catch(error => {
+        console.log(error);
+      })
+  }
+  const pedidoDelete = async () => {
+    await axios.delete(baseUrl + "/" + alunoSelecionado.id)
+      .then(response => {
+        setData(data.filter(aluno => aluno.id !== response.data));
+        setUpdateData(true);
+        abrirFecharModalExcluir();
+      }).catch(error => {
+        console.log(error);
+      })
+  }
+
+  const seleciionarAluno = (aluno, opcao) => {
+    setAlunoSelecionado(aluno);
+    (opcao === "Editar") ?
+      abrirFecharModalEditar() : abrirFecharModalExcluir();
+  }
   useEffect(() => {
-     {
+    if (updateData) {
       pedidoGet();
+      setUpdateData(false);
     }
-  })
+  }, [updateData])
 
   return (
-    <div className="App">
+    <div className="aluno-container">
       <br />
       <h3>Cadastro de Alunos</h3>
       <header>
         <img src={logoCadastro} alt='Cadastro' />
-        <button className="btn btn-success">Incluir Novo Aluno</button>
+        <button className="btn btn-success" onClick={() => abrirFecharModalIncluir()}>Incluir Novo Aluno</button>
       </header>
       <table className="table table-bordered">
         <thead>
@@ -51,13 +128,75 @@ function App() {
               <td>{aluno.email}</td>
               <td>{aluno.idade}</td>
               <td>
-                <button className="btn btn-primary" >Editar</button> {"  "}
-                <button className="btn btn-danger" >Excluir</button>
+                <button className="btn btn-primary" onClick={() => seleciionarAluno(aluno, "Editar")} >Editar</button> {"  "}
+                <button className="btn btn-danger" onClick={() => seleciionarAluno(aluno, "Excluir")}>Excluir</button>
               </td>
             </tr>
           ))}
         </tbody>
       </table>
+      <Modal isOpen={modalIncluir}>
+        <ModalHeader>Incluir Aluno</ModalHeader>
+        <ModalBody>
+          <div className="form-group">
+            <label>Nome: </label>
+            <br />
+            <input type="text" className="form-control" name="nome" onChange={handleChange} />
+            <br />
+            <label>Email: </label>
+            <br />
+            <input type="text" className="form-control" name="email" onChange={handleChange} />
+            <label>Idade: </label>
+            <br />
+            <input type="text" className="form-control" name="idade" onChange={handleChange} />
+            <br />
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <button className="btn btn-primary" onClick={() => pedidoPost()} >Incluir</button>{"  "}
+          <button className="btn btn-danger" onClick={() => abrirFecharModalIncluir()}>Cancelar</button>
+        </ModalFooter>
+      </Modal>
+
+      <Modal isOpen={modalEditar}>
+        <ModalHeader>Editar Aluno</ModalHeader>
+        <ModalBody>
+          <div className="form-group">
+            <label>Id: </label>
+            <input type="text" className="form-control" name="nome" readOnly value={alunoSelecionado && alunoSelecionado.id} />
+            <br />
+            <label>Nome: </label>
+            <br />
+            <input type="text" className="form-control" name="nome" onChange={handleChange}
+              value={alunoSelecionado && alunoSelecionado.nome} />
+
+            <br />
+            <label>Email: </label>
+            <br />
+            <input type="text" className="form-control" name="email" onChange={handleChange}
+              value={alunoSelecionado && alunoSelecionado.email} />
+
+            <label>Idade: </label>
+            <br />
+            <input type="text" className="form-control" name="idade" onChange={handleChange}
+              value={alunoSelecionado && alunoSelecionado.idade} />
+            <br />
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <button className="btn btn-primary" onClick={() => pedidoPut()} >Editar</button>{"  "}
+          <button className="btn btn-danger" onClick={() => abrirFecharModalEditar()}>Cancelar</button>
+        </ModalFooter>
+      </Modal>
+      <Modal isOpen={modalExcluir}>
+        <ModalBody>
+          Confirma a exclusão deste(a) aluno(a) : {alunoSelecionado && alunoSelecionado.nome}?
+          <ModalFooter>
+            <button className="btn btn-danger" onClick={() => pedidoDelete()}> Sim </button>
+            <button className="btn btn-secondary" onClick={() => abrirFecharModalExcluir()}> Não </button>
+          </ModalFooter>
+        </ModalBody>
+      </Modal>
     </div>
   );
 }
